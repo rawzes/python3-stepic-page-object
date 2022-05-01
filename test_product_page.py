0a0b7f3c8f225db12ece2e8c5da93787b3682f47
Roman Razumeyko <rawzes@gmail.com>
2022-05-01 21:56:52 +0300

    Added parametrization

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
 from pages.product_page import ProductPage
+import pytest
+
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+links = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+links[7] = pytest.param(links[7], marks=pytest.mark.xfail(reason='Known issue mark'))
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_item_to_basket()

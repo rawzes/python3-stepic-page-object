166463d8296590778cff393b773a66b2d576a61f
Roman Razumeyko <rawzes@gmail.com>
2022-05-01 12:33:41 +0300

    Added LoginPage initialization inside test

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,13 @@
 from pages.main_page import MainPage
-
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.should_be_login_link()
-    login_page = main_page.go_to_login_page()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_form()
     login_page.should_be_register_form()
     login_page.should_be_login_url()
